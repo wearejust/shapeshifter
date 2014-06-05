054d7d2604ae0cedb201061bc29e4f386552aaea
Jasper van der Kamp <jasper.vanderkamp@wearejust.com>
2014-06-05 16:54:28 +0200

    no message

--- src/Just/Shapeshifter/Attributes/ColorAttribute/view/ColorAttribute.blade.php
+++ src/Just/Shapeshifter/Attributes/ColorAttribute/view/ColorAttribute.blade.php
@@ -1,12 +1,12 @@
-<div class="form-group">
-    <div class="form-label col-3">
-        {{ Form::label($name, $label) }}
-    </div>
-    <div class="form-field col-9">
-        <div class="form-control">
-            {{ Form::text($name, null, array('class' => 'colorpicker')) }}
-        </div>
+<label class="form-group">
+    <span class="form-label">
+        {{$label}}
+    </span>
+    <span class="form-field">
+        <span class="form-control">
+            {{ Form::text($name, null, array('class' => 'form-field-content colorpicker')) }}
+            <span class="form-group-highlight"></span>
+        </span>
         @include('shapeshifter::layouts.helptext')
-    </div>
-</div>
-
+    </span>
+</label>
