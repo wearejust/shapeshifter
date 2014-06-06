58b1b94196ed92145879085e87968bf239f52ec3
Jasper van der Kamp <jasper.vanderkamp@wearejust.com>
2014-06-06 16:36:22 +0200

    no message

--- src/views/relations/ManyToManyFacebookRelation.blade.php
+++ src/views/relations/ManyToManyFacebookRelation.blade.php
@@ -4,16 +4,16 @@
     </div>
     <div class="form-field">
         <div class="form-control">
-            <div class="">
-            {{
-                Form::text($name, '', array(
-                    isset($flags[0]) && $flags[0] == 'readonly' ? 'readonly' : '',
-                    'class' => 'form-control tokeninput',
-                    'id' => $name,
-                    'data-prepopulate' => $results,
-                    'data-allresults' => $all
-                ))
-            }}
+            <div class="input" style="cursor: text;">
+                {{
+                    Form::text($name, '', array(
+                        isset($flags[0]) && $flags[0] == 'readonly' ? 'readonly' : '',
+                        'class' => 'form-control tokeninput',
+                        'id' => $name,
+                        'data-prepopulate' => $results,
+                        'data-allresults' => $all
+                    ))
+                }}
             </div>
         </div>
         @include('shapeshifter::layouts.helptext')
