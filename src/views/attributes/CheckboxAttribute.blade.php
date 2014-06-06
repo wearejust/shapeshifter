3af96f707f4bfb14aa69a3bacf63ded69c336587
Jasper van der Kamp <jasper.vanderkamp@wearejust.com>
2014-06-06 15:51:08 +0200

    no message

--- src/views/attributes/CheckboxAttribute.blade.php
+++ src/views/attributes/CheckboxAttribute.blade.php
@@ -1,9 +1,10 @@
-<div class="form-group">
-	<div class="form-field checkbox">
-		<div class="group">
-	        {{ Form::checkbox($name, 1, Input::get($name) ?: null, array('class' => 'single-checkbox', 'id' => $name)) }}
-	        {{ Form::label($name, $label, array('class' => 'section-start section-end single-checkbox-label', 'for' => $name)) }}
-        </div>
-        @include('shapeshifter::layouts.helptext')
-    </div>
-</div>
+<label class="form-group">
+    <span class="form-label">
+        {{ $label }}
+    </span>
+    <span class="form-field">
+		{{ Form::checkbox($name, 1, Input::get($name) ?: null, array('class' => 'single-checkbox', 'id' => $name)) }}
+        <span class="form-group-highlight"></span>
+	    @include('shapeshifter::layouts.helptext')
+    </span>
+</label>
