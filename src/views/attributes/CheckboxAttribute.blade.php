71047302d57e56d565f1d26250d5ddcd98793f85
Jasper van der Kamp <jasper.vanderkamp@wearejust.com>
2014-06-10 11:22:08 +0200

    no message

--- src/views/attributes/CheckboxAttribute.blade.php
+++ src/views/attributes/CheckboxAttribute.blade.php
@@ -3,7 +3,7 @@
         {{ $label }}
     </span>
     <span class="form-field">
-		{{ Form::checkbox($name, 1, Input::get($name) ?: null, array('class' => 'form-field-content single-checkbox', 'id' => $name)) }}
+		{{ Form::checkbox($name, 1, Input::get($name) ?: null, array('class' => 'form-single-checkbox', 'id' => $name)) }}
         <span class="form-group-highlight"></span>
 	    @include('shapeshifter::layouts.helptext')
     </span>
