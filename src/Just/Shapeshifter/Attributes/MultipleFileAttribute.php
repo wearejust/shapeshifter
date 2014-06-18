f53b47dc71046adf6891885eea532c37e34d6cb4
Jasper van der Kamp <jasper.vanderkamp@wearejust.com>
2014-06-18 11:31:07 +0200

    no message

---
+++ src/Just/Shapeshifter/Attributes/MultipleFileAttribute.php
@@ -0,0 +1,19 @@
+<?php namespace Just\Shapeshifter\Attributes;
+
+
+/**
+* MultipleFileAttribute
+*
+* @uses     Attribute
+*
+* @category Category
+* @package  Package
+* @author   JUST BV
+* @link     http://wearejust.com/
+*/
+class MultipleFileAttribute extends Attribute implements iAttributeInterface
+{
+    
+}
+
+?>
