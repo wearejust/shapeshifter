fc85cbc58be4b1b1219aaaecebe700f3a1b739fa
Jasper van der Kamp <jasper.vanderkamp@wearejust.com>
2014-06-19 12:35:33 +0200

    no message

--- src/views/attributes/DateAttribute.blade.php
+++ src/views/attributes/DateAttribute.blade.php
@@ -4,7 +4,7 @@
     </span>
     <span class="form-field">
     	<span class="form-control form-field-short">
-    		{{ Form::text($name, null, array('class' => 'form-field-content datepicker', 'placeholder' => 'dd-mm-jjjj', 'autocorrect' => 'off')) }}
+    		{{ Form::text($name, null, array('class' => 'form-field-content datepicker', 'pattern' => '(?:0[1-9]|1[0-9]|2[0-9])|(?:(?!02)(?:0[1-9]|1[0-2])-(?:30))|(?:(?:0[13578]|1[02])-31))-(?:(?:0[1-9]|1[0-2])-(?:19|20)[0-9]{2}', 'placeholder' => 'dd-mm-jjjj', 'autocorrect' => 'off')) }}
             <span class="form-group-highlight"></span>
         </span>
         @include('shapeshifter::layouts.helptext')
