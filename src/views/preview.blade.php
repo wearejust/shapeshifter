0cdb23fe553a8ad1f8d54513b1706650a320fca1
Jasper van der Kamp <jasper.vanderkamp@wearejust.com>
2014-07-02 14:30:41 +0200

    no message

--- src/views/preview.blade.php
+++ src/views/preview.blade.php
@@ -3,6 +3,7 @@
 <style>
     * { margin: 0; padding: 0; }
     html, body { height: 100%; }
+    html, button, datalist, input, select, textarea { -webkit-font-smoothing: antialiased; font-smoothing: antialiased; text-rendering: optimizeLegibility; }
     body { font-family: HelveticaNeue, Arial, sans-serif; font-size: 14px; }
     p { background-color: #333; color: #fff; left: 0; line-height: normal; padding: 1.1em 1.5em; position: fixed; right: 0; top: 0; }
     div { bottom: 0; left: 0; position: fixed; right: 0; top: 3.357em; }
