6d74da70dc10c0e35668b4030328b5cfb3fce7de
Jasper van der Kamp <jasper.vanderkamp@wearejust.com>
2014-07-14 16:30:10 +0200

    no message

--- src/views/attributes/RadiobuttonAttribute.blade.php
+++ src/views/attributes/RadiobuttonAttribute.blade.php
@@ -7,7 +7,7 @@
 			<div class="form-options">
 				<ul class="list form-options-content">
 					@foreach ($values as $value)
-			        <li class="form-option form-option-grid-2 form-option-grid-3"> <!-- grid-2 gebruiken voor 2 items naast elkaar, grid-3 voor 3 (op groot scherm) -->
+			        <li class="form-option form-option-grid-2 form-option-grid-3"> <!-- form-option-grid-2 gebruiken voor 2 items naast elkaar, form-option-grid-3 voor 3 (op groot scherm) -->
 				        <div class="form-option-content">
 					        {{ Form::radio($name, $value, null, array('class' => 'form-option-field' . ($required?' js-required':''), 'id' => 'radio-' . $value)) }}
 					        <label class="form-option-label" for="radio-{{ $value }}">
