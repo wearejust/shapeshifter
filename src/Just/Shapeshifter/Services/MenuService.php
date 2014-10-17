ed915cb1a8f3f39cd7a3d21826d9ea35e1ce1815
John in 't Hout <john.inthout@wearejust.com>
2014-10-17 16:47:34 +0200

    fixed menu services bug.

--- src/Just/Shapeshifter/Services/MenuService.php
+++ src/Just/Shapeshifter/Services/MenuService.php
@@ -5,50 +5,51 @@ use Illuminate\Support\Collection;
 
 class MenuService
 {
-    /**
-     * @var Application
-     */
-    private $app;
+	/**
+	 * @var Application
+	 */
+	public $app;
 
-    /**
-     * @var Collection
-     */
-    private $collection;
+	/**
+	 * @var Collection
+	 */
+	public $collection;
 
-    public function __construct(Application $app, Collection $collection)
-    {
-        $this->app = $app;
-        $this->collection = $collection;
-    }
+	public function __construct(Application $app, Collection $collection)
+	{
+		$this->app = $app;
+		$this->collection = $collection;
+	}
 
-    public function generateMenu()
-    {
-	    $menuConfig = $this->app['config']->get('shapeshifter::config.menu');
-		$activeUser = \Sentry::getUser();
-	    if($activeUser->isSuperUser()) {
-		    $menuConfig = array_merge($menuConfig, $this->app['config']->get('shapeshifter::config.super-admin-menu'));
-	    }
+	public function generateMenu()
+	{
+		$config = $this->collection->make($this->app['config']->get('shapeshifter::config.menu'));
 
-        $config = $this->collection->make($menuConfig);
+		$reference = $this;
+		$config->transform(function($config) use ($reference)
+		{
+			$config['active'] = $reference->app['request']->segment(2) === $config['url'];
 
-        $config->transform(function($config)
-        {
-            $config['active'] = $this->app['request']->segment(2) === $config['url'];
-			if(isset($config['children']) && $config['children'] > 0) {
-				$config['children'] = $this->collection->make($config['children']);
-				$config['children']->transform(function($child) use (&$config)
-				{
-					$child['active'] = $this->app['request']->segment(2) === $child['url'];
-					if ($child['active']) {
+			$config['children'] = $reference->collection->make($config['children']);
+			$config['children']->transform(function($child) use (&$config, $reference)
+			{
+
+				$child['active'] = ($reference->app['request']->segment(2) === $child['url']);
+				if ($child['active']) {
+					$config['active'] = true;
+				}else {
+					$child['active'] = \Session::get('category') == last(explode('=', last(explode('?', $child['url']))));
+					if ($child['active']){
 						$config['active'] = true;
 					}
+				}
 
-					return $child;
-				});
-			}
-            return $config;
-        });
+				return $child;
+			});
 
-        return $config;
-    }
+			return $config;
+		});
+
+		return $config;
+	}
 }
