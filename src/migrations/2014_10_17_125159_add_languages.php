562c6f424db4d631a5d34b3471336e098400b465
John in 't Hout <john.inthout@wearejust.com>
2014-10-28 11:40:44 +0100

    Updated migration file.

--- src/migrations/2014_10_17_125159_add_languages.php
+++ src/migrations/2014_10_17_125159_add_languages.php
@@ -4,27 +4,28 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 use Just\Shapeshifter\Core\Models\Language;
 
-class AddLanguages extends Migration {
+class AddLanguages extends Migration
+{
 
 	/**
 	 * Run the migrations.
 	 *
 	 * @return void
 	 */
-	public function up()
+	public function up ()
 	{
 		Language::create([
 			                 'short_code' => 'nl',
 			                 'active'     => true,
 			                 'name'       => 'Nederlands',
-			                 'default'    => true,
+			                 'sortorder'  => 1
 		                 ]);
 
 		Language::create([
 			                 'short_code' => 'en',
 			                 'active'     => false,
 			                 'name'       => 'English',
-			                 'default'    => false,
+			                 'sortorder'  => 2
 		                 ]);
 	}
 
@@ -33,7 +34,7 @@ class AddLanguages extends Migration {
 	 *
 	 * @return void
 	 */
-	public function down()
+	public function down ()
 	{
 		DB::table('languages')->truncate();
 	}
