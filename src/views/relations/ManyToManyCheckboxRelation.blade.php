02d260fad3bf19af8af313f89a55392a928249bd
Emre Koc <emre.koc@wearejust.com>
2014-11-18 14:39:56 +0100

    ManyToManyChecboxRelation missing form-group-content

--- src/views/relations/ManyToManyCheckboxRelation.blade.php
+++ src/views/relations/ManyToManyCheckboxRelation.blade.php
@@ -1,20 +1,22 @@
 <div class="form-group form-group-options">
-    <fieldset>
-        <legend class="form-label wrap section-end">
-            <span class="form-legend">{{ $label }}</span>
-        </legend>
-        <div class="form-field radio">
-            <div class="form-options">
-                @foreach ($all as $value => $label)
-                <div class="section form-option">
-                    {{ Form::checkbox($name . '['.$value.']', 1, in_array($value, $results), array('class' => 'form-option-field', 'id' => 'multiple-checkbox-' . $name . '-' .$value)) }}
-                    <label class="form-option-label" for="multiple-checkbox-{{ $name }}-{{ $value }}">
-                        {{ $label }}
-                    </label>
+    <span class="form-group-content">
+        <fieldset>
+            <legend class="form-label wrap section-end">
+                <span class="form-legend">{{ $label }}</span>
+            </legend>
+            <div class="form-field radio">
+                <div class="form-options">
+                    @foreach ($all as $value => $label)
+                    <div class="section form-option">
+                        {{ Form::checkbox($name . '['.$value.']', 1, in_array($value, $results), array('class' => 'form-option-field', 'id' => 'multiple-checkbox-' . $name . '-' .$value)) }}
+                        <label class="form-option-label" for="multiple-checkbox-{{ $name }}-{{ $value }}">
+                            {{ $label }}
+                        </label>
+                    </div>
+                    @endforeach
                 </div>
-                @endforeach
+                @include('shapeshifter::layouts.helptext')
             </div>
-            @include('shapeshifter::layouts.helptext')
-        </div>
-    </fieldset>
+        </fieldset>
+    </span>
 </div>
