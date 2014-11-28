92030f666b204b0af258b86456cdda6196cc401b
Emre Koc <emre.koc@wearejust.com>
2014-11-28 09:29:28 +0100

    Table-row-editoable click

--- public/js/main.js
+++ public/js/main.js
@@ -932,11 +932,13 @@ var SortableTable = function (options, table) {
 
     this.items = this.tbody.children();
     this.items.filter('.table-row-editable').find('td:not(.table-order, .table-control)').on('click', function (e) {
-        var url = $(e.currentTarget).closest('.table-row').attr('data-edit-href');
-        if (e.button || e.ctrlKey || e.metaKey) {
-            window.open(url);
-        } else {
-            window.location = url;
+        if (!$(e.target).filter('a').length) {
+            var url = $(e.currentTarget).closest('.table-row').attr('data-edit-href');
+            if (e.button || e.ctrlKey || e.metaKey) {
+                window.open(url);
+            } else {
+                window.location = url;
+            }
         }
     });
 
