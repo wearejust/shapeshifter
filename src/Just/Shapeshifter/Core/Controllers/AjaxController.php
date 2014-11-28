f9f7021464e8dc493b31afcdcb453c4da942108e
John in 't Hout <john.inthout@wearejust.com>
2014-11-28 10:18:04 +0100

    Fixed the upload filename at upload.

--- src/Just/Shapeshifter/Core/Controllers/AjaxController.php
+++ src/Just/Shapeshifter/Core/Controllers/AjaxController.php
@@ -9,76 +9,76 @@ use Response;
 
 class AjaxController extends Controller
 {
-    protected function sortorderChange()
-    {
-        if ( ! $model = ucfirst(Input::get('model')) )
-        {
-            return Response::json( array('You have to specify an model') );
-        }
+	protected function sortorderChange()
+	{
+		if ( ! $model = ucfirst(Input::get('model')) )
+		{
+			return Response::json( array('You have to specify an model') );
+		}
 
-        $model = new $model;
+		$model = new $model;
 
-        if ($model instanceof BasePresenter) $model = $model->getResource();
+		if ($model instanceof BasePresenter) $model = $model->getResource();
 
-        $orderByColumn = isset($model->orderby[0]) ? $model->orderby[0] : 'sortorder';
-        $orderByMode = isset($model->orderby[1]) ? $model->orderby[1] : 'ASC';
+		$orderByColumn = isset($model->orderby[0]) ? $model->orderby[0] : 'sortorder';
+		$orderByMode = isset($model->orderby[1]) ? $model->orderby[1] : 'ASC';
 
-        $segments = array_reverse(explode('/', Input::get('url')));
-        foreach ($segments as $segment) {
-            if (is_numeric($segment)) $numeric = $segment;
-        }
+		$segments = array_reverse(explode('/', Input::get('url')));
+		foreach ($segments as $segment) {
+			if (is_numeric($segment)) $numeric = $segment;
+		}
 
 
-        if (Input::has('relation') && isset($numeric))
-        {
-            $model = $model->where('id', $numeric)->first();
+		if (Input::has('relation') && isset($numeric))
+		{
+			$model = $model->where('id', $numeric)->first();
 
-            $records = $model->{Input::get('relation')}(function($q) use($orderByColumn, $orderByMode) {
-                $q->orderBy( $orderByColumn, $orderByMode );
-            })->get();
-        }
-        else
-        {
-            $records = $model::orderBy( $orderByColumn, $orderByMode )->get();
-        }
+			$records = $model->{Input::get('relation')}(function($q) use($orderByColumn, $orderByMode) {
+				$q->orderBy( $orderByColumn, $orderByMode );
+			})->get();
+		}
+		else
+		{
+			$records = $model::orderBy( $orderByColumn, $orderByMode )->get();
+		}
 
-        $sortorder = 1;
-        foreach (Input::get('order') as $new)
-        {
-            foreach ($records as $rec)
-            {
-                if ($new == $rec->id)
-                {
-                    $rec->sortorder = $sortorder;
-                    $rec->save();
+		$sortorder = 1;
+		foreach (Input::get('order') as $new)
+		{
+			foreach ($records as $rec)
+			{
+				if ($new == $rec->id)
+				{
+					$rec->sortorder = $sortorder;
+					$rec->save();
 
-                    $sortorder++;
+					$sortorder++;
 
-                    break;
-                }
-            }
+					break;
+				}
+			}
 
-        }
+		}
 
-        return Response::json( array('message' => (string)Notification::successInstant('De volgorde is opgeslagen.'), 'status' => 200) );
-    }
+		return Response::json( array('message' => (string)Notification::successInstant('De volgorde is opgeslagen.'), 'status' => 200) );
+	}
 
-    protected function upload()
-    {
-        $storageDir = Input::get('storagedir');
-        $input = Input::file('files');
-        $files = new Collection();
+	protected function upload()
+	{
+		$storageDir = Input::get('storagedir');
+		$input = Input::file('files');
+		$files = new Collection();
 
-        foreach ($input as $file) {
-            if ($file->isValid()) {
-                $name = $file->getClientOriginalName();
-                $file->move(public_path() . $storageDir, $name);
-                $files[$name] = $storageDir . $name;
-            }
-        }
+		foreach ($input as $file) {
+			if ($file->isValid()) {
+				$name = \Str::slug(pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME));
+				$extension = $file->getClientOriginalExtension();
+				$file_name = $name . '.' . $extension;
+				$file->move(public_path() . $storageDir, $file_name);
+				$files[$file_name] = $storageDir . $file_name;
+			}
+		}
 
-        return $files;
-    }
+		return $files;
+	}
 }
-
-?>
