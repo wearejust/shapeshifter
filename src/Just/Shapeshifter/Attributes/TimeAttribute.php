bd01df4a6126ae2490014696b2abd5659fa71bee
Bram Bruurs <bram.bruurs@wearejust.com>
2015-03-02 15:07:34 +0100

    time attribute 2e update

--- src/Just/Shapeshifter/Attributes/TimeAttribute.php
+++ src/Just/Shapeshifter/Attributes/TimeAttribute.php
@@ -36,7 +36,8 @@ class TimeAttribute extends Attribute implements iAttributeInterface
     {
 		$this->name = $name;
 		$this->flags = $flags;
-        $this->values = array(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,30,21,22,23);
+        $this->values = array("00"=>"00","01"=>"01","02"=>"02","03"=>"03","04"=>"04","05"=>"05","06"=>"06","07"=>"07","08"=>"08","09"=>"09","10"=>"10","11"=>"11","12"=>"12","13"=>"13","14"=>"14","15"=>"15","16"=>"16","17"=>"17","18"=>"18","19"=>"19", "20"=>"20","21"=>"21","22"=>"22","23"=>"23");
+        $this->values2 = array("00"=>"00","05"=>"05","10"=>"10","15"=>"15","20"=>"20","25"=>"25","30"=>"30","35"=>"35","40"=>"40","45"=>"45","50"=>"50","55"=>"55");
 	}
 
     /**
@@ -47,9 +48,26 @@ class TimeAttribute extends Attribute implements iAttributeInterface
      */
     public function getEditValue()
     {
+
+        //dd($this->value);
+        $delen = explode(":", $this->value);
+
+
+
+
+        $this->value = $delen[0];
+        if(isset($delen[1])) {
+            $this->value2 = $delen[1];
+        }
+
+        //dd($delen);
+        //
+        return $this->value;
+
         if ( ! $this->value ) return null;
 
-        return $this->value;
+
+        return array($delen[0], $delen[1]);
     }
 
     /**
@@ -60,6 +78,14 @@ class TimeAttribute extends Attribute implements iAttributeInterface
      */
     public function getDisplayValue()
     {
+        //dd($this->value);
+        return $this->value;
+        $delen = explode(":", $this->value);
+        $this->value = $delen[0];
+        $this->value2 = $delen[1];
+
+
+
         if (array_key_exists($this->value, $this->values)) 
         {
             return $this->values[$this->value];
@@ -67,4 +93,15 @@ class TimeAttribute extends Attribute implements iAttributeInterface
 
         return 'Geen';
     }
+
+    public function getSaveValue()
+    {
+        //dd(\Input::all());
+
+        return \Input::get($this->name ).":".\Input::get($this->name."_min" );
+
+        //return $this->value . $this->value2";
+    }
+
+
 }
--- src/views/attributes/TimeAttribute.blade.php
+++ src/views/attributes/TimeAttribute.blade.php
@@ -6,7 +6,8 @@
     	<span class="form-field" style="float: right;">
             <span class="form-control form-field-medium">
                 <span class="module-1" style="margin-right: 0;">
-                    {{ Form::select($name, $values, null, array('class' => 'form-field-content' . ($required?' js-required':''), 'id' => $name)) }}
+                     {{ Form::select($name, $values, null, array('style'=> "width: 80px;", 'class' => 'form-field-content' . ($required?' js-required':''), 'id' => $name)) }}
+                    {{ Form::select($name."_min", $values2, $value2, array('style'=> "width: 80px;", 'class' => 'form-field-content' . ($required?' js-required':''), 'id' => $name."_min")) }}
                     <span class="form-group-highlight"></span>
                 </span>
             </span>
