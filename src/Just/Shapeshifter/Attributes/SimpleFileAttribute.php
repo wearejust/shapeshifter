15de99aa11e768a8d530b636c761f268348adeb4
Bram Bruurs <bram.bruurs@wearejust.com>
2015-04-30 10:28:36 +0200

    no not overwrite existing files

--- src/Just/Shapeshifter/Attributes/SimpleFileAttribute.php
+++ src/Just/Shapeshifter/Attributes/SimpleFileAttribute.php
@@ -200,12 +200,63 @@ class SimpleFileAttribute extends Attribute implements iAttributeInterface
             $filename = Input::file($this->name)->getClientOriginalName();
             $filename = Str::slug(str_replace($extension, '', $filename));
 
-            Input::file($this->name)->move($this->absoluteStorageDir, ($filename . $extension));
+
+            if(file_exists($this->absoluteStorageDir.$filename. $extension)){
+                $new_name = "";
+                $teller = 1;
+                $base_name = str_replace($extension, "", $filename);
+                while($new_name == "") {
+
+
+                    if(file_exists($this->absoluteStorageDir.$base_name."_".$teller.$extension)){
+
+                        //echo("bestond al".$base_name);
+                        $teller ++;
+
+                    }
+                    else {
+                        //echo "bestaat nog niet: ".$this->absoluteStorageDir.$base_name."_".$teller.$extension;
+                        move_uploaded_file(Input::file($this->name)->getRealPath(), $this->absoluteStorageDir .$base_name."_".$teller . $extension);
+                        if($teller>1) {
+                            $this->value = $base_name . "_" . ($teller - 1) . $extension;
+                        }
+                        else {
+                            $this->value = $base_name . $extension;
+                        }
+                        return;
+
+                       $new_name =  $base_name;
+                    }
+
+                    //echo ('check voor ' . $this->absoluteStorageDir.$base_name."_".$teller.$extension);
+
+
+                }
+                $filename = $new_name."_".$teller;
+            }
+            else {
+                //die("bestond nog niet, wordt ".$filename);
+            }
+
+
+            //die("nu naam bedacht, ".$filename);
+
+
+
+            //Input::file($this->name)->move($this->absoluteStorageDir, ($filename . $extension));
+            //die("move " . Input::file($this->name)->getRealPath() . " Naar " . $this->absoluteStorageDir. "/" .$filename . $extension );
+            move_uploaded_file(Input::file($this->name)->getRealPath(), $this->absoluteStorageDir .$filename . $extension);
+
+            if(isset($new_name)) {
+                //$filename = $new_name."_".$teller-1;
+            }
+
 
             $this->value = $filename . $extension;
+            return "";
         }
-
-        return '';
+        //die('uiteindelijk geworden' . $this->value);
+        return "";
     }
 
     /**
