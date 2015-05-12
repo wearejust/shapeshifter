8c0685e2d93e21011915a4343e1d63e2bca9a24c
Bram Bruurs <bram.bruurs@wearejust.com>
2015-05-12 09:25:06 +0200

    fix for default value

--- src/views/attributes/TextAttribute.blade.php
+++ src/views/attributes/TextAttribute.blade.php
@@ -6,10 +6,10 @@
         </span>
         <span class="form-field">
         	<span class="form-control">
-                @if(isset($flags['default_value']))
+                @if(isset($flags['default_value']) && substr(URL::current(), -6) == "create")
     	           {{ Form::input($type, $name, (isset($translation_value)) ? $translation_value : $flags['default_value'], array('class' => 'form-field-content' . ($required ?' js-required':''), 'id' => $name)) }}
                 @else
-                    {{ Form::input($type, $name, (isset($translation_value)) ? $translation_value : '', array('class' => 'form-field-content' . ($required ?' js-required':''), 'id' => $name)) }}
+                    {{ Form::input($type, $name, (isset($translation_value)) ? $translation_value : null, array('class' => 'form-field-content' . ($required ?' js-required':''), 'id' => $name)) }}
                 @endif
     	        <span class="form-group-highlight"></span>
     	    </span>
