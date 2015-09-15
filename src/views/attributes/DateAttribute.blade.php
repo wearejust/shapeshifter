e3980b37e6eed7f4587a9c656a6fb65b7d16702d
Emre Koc <emre.koc@wearejust.com>
2015-09-15 14:57:47 +0200

    Fix regex pattern on DateAttribute

--- src/views/attributes/DateAttribute.blade.php
+++ src/views/attributes/DateAttribute.blade.php
@@ -6,7 +6,7 @@
         <span class="form-field">
             <span class="form-control form-field-short">
                 <span class="module-1">
-                    {{ Form::text($name, null, array('class' => 'form-field-content datepicker' . ($required?' js-required':''), 'id' => $name, 'pattern' => '(?:0[1-9]|1[0-9]|2[0-9])|(?:(?!02)(?:0[1-9]|1[0-2])-(?:30))|(?:(?:0[13578]|1[02])-31))-(?:(?:0[1-9]|1[0-2])-(?:19|20)[0-9]{2}', /*'placeholder' => 'dd-mm-jjjj', */'autocorrect' => 'off')) }}
+                    {{ Form::text($name, null, array('class' => 'form-field-contsdaf aswdfsdaent datepicker' . ($required?' js-required':''), 'id' => $name, 'pattern' => '(0[1-9]|1[0-9]|2[0-9]|3[0-1])-(0[1-9]|1[0-2])-([0-9]{4})', /*'placeholder' => 'dd-mm-jjjj', */'autocorrect' => 'off')) }}
                 </span>
                 <span class="form-group-highlight"></span>
             </span>
