92b712b95b0bea119e6ad94f6b53e67bc974287d
Cees van Egmond <cees.vanegmond@wearejust.com>
2016-02-02 15:58:39 +0100

    Feature: Added new LatLongAttribute that contains a full lat;long Also, added a special trait that can be used to simplify getter management

---
+++ src/Just/Shapeshifter/Core/Models/Traits/LatLng.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Just\Shapeshifter\Core\Models\Traits;
+
+use InvalidArgumentException;
+
+trait LatLng
+{
+    /**
+     * @var string
+     */
+    private $latLongField = 'latlng';
+
+    /**
+     * @param string|null $field
+     * @return string
+     */
+    public function getLatitude($field = null)
+    {
+        return $this->splitLatLong($field, 0);
+    }
+
+    /**
+     * @param string|null $field
+     * @return string
+     */
+    public function getLongitude($field = null)
+    {
+        return $this->splitLatLong($field, 1);
+    }
+
+    /**
+     * @param string|null $field
+     * @param int $index
+     * @return array
+     */
+    private function splitLatLong($field, $index)
+    {
+        $field = $field ?: $this->latLongField;
+        $explode = explode($this->{$field});
+
+        if (! array_key_exists($index, $explode) || count($explode) !== 2) {
+            throw new InvalidArgumentException(sprintf('Invalid latlong string given on field [%s]', $field));
+        }
+
+        return (float) $explode[$index];
+    }
+}
