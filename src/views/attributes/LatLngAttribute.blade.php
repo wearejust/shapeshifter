9489d454a8dfb25bbf151a02df99a1c8b1327850
Emre Koc <emre.koc@wearejust.com>
2016-02-04 10:35:57 +0100

    Bugfix: height of LatLngAttribute map

--- src/views/attributes/LatLngAttribute.blade.php
+++ src/views/attributes/LatLngAttribute.blade.php
@@ -6,8 +6,14 @@
         </span>
         <span class="form-field">
         	<span class="form-control">
-                <div class="form-map" style="padding-bottom: 56.25%;"></div>
-                {{ Form::input('text', $name, (isset($translation_value)) ? $translation_value : (isset($flags['default_value']) ? $flags['default_value'] : null), array('class' => 'form-field-content' . ($required ?' js-required':''), 'id' => $name)) }}
+                <div class="form-field-content" style="padding-bottom: 56.25%;">
+                    <div class="js-latlngattribute-map" style="position: absolute; left: 0; right: 0; top: 0; bottom: 0;"></div>
+                </div>
+                @if (!in_array('compact', $flags))
+                    {{ Form::input('text', $name . '-lat', null, array('class' => 'js-latlngattribute-lat form-field-content', 'placeholder' => 'Latitude')) }}
+                    {{ Form::input('text', $name . '-lon', null, array('class' => 'js-latlngattribute-lng form-field-content', 'placeholder' => 'Longitude')) }}
+                @endif
+                {{ Form::hidden($name, (isset($translation_value)) ? $translation_value : (isset($flags['default_value']) ? $flags['default_value'] : null), array('class' => 'js-latlngattribute-input form-field-content' . ($required ?' js-required':''), 'id' => $name)) }}
     	    </span>
     	    @include('shapeshifter::layouts.helptext')
         </span>
