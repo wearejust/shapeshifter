edd8920aa3cb01c75d67cf5cfeba9183c3ed916e
Emre Koc <emre.koc@wearejust.com>
2016-11-17 12:24:54 +0100

    Bugfix: add checks for MediumAttribute scripts to prevent loading multiple times

--- resources/views/attributes/MediumAttribute.blade.php
+++ resources/views/attributes/MediumAttribute.blade.php
@@ -14,36 +14,43 @@
 <br>
 @section('scripts')
     @parent
-    <script src="//cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.5/handlebars.runtime.min.js"></script>
-    <script src="{{ asset('packages/just/shapeshifter/js/attributes/medium/jquery.ui.widget.js') }} "></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/blueimp-file-upload/9.5.7/jquery.iframe-transport.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/blueimp-file-upload/9.5.7/jquery.fileupload.js"></script>
 
-    <script>
-        var mediumAttribute = {
-            'config' : {
-                iframelyApiKey: ''
-            },
-            'translations': {
-                captionPlaceholder: "{{ trans('shapeshifter::editor.captionPlaceholder') }}",
-                embedPlaceholder: "{{ trans('shapeshifter::editor.embedPlaceholder') }}",
-            }
-        };
-    </script>
+    @if (!isset($_SERVER['SCRIPTS_LOADED_MEDIUM']))
+        <script src="//cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.5/handlebars.runtime.min.js"></script>
+        <script src="{{ asset('packages/just/shapeshifter/js/attributes/medium/jquery.ui.widget.js') }} "></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/blueimp-file-upload/9.5.7/jquery.iframe-transport.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/blueimp-file-upload/9.5.7/jquery.fileupload.js"></script>
 
-    <script src="{{ asset('packages/just/shapeshifter/js/attributes/medium/medium/js/medium-editor.js') }}"></script>
-    <script src="{{ asset('packages/just/shapeshifter/js/attributes/medium/medium-editor-insert-plugin/dist/js/medium-editor-insert-plugin.min.js') }}"></script>
-    <script src="{{ asset('packages/just/shapeshifter/js/attributes/medium/js/filebrowser.js') }}"></script>
-    <script src="{{ asset('packages/just/shapeshifter/js/attributes/medium/medium/medium-button.js') }}"></script>
-    <script src="{{ asset('packages/just/shapeshifter/js/attributes/medium/extensions/pastehandler.js') }}"></script>
-    <script src="{{ asset('packages/just/shapeshifter/js/attributes/medium/medium.js') }}"></script>
+        <script>
+            var mediumAttribute = {
+                'config' : {
+                    iframelyApiKey: ''
+                },
+                'translations': {
+                    captionPlaceholder: "{{ trans('shapeshifter::editor.captionPlaceholder') }}",
+                    embedPlaceholder: "{{ trans('shapeshifter::editor.embedPlaceholder') }}",
+                }
+            };
+        </script>
+
+        <script src="{{ asset('packages/just/shapeshifter/js/attributes/medium/medium/js/medium-editor.js') }}"></script>
+        <script src="{{ asset('packages/just/shapeshifter/js/attributes/medium/medium-editor-insert-plugin/dist/js/medium-editor-insert-plugin.min.js') }}"></script>
+        <script src="{{ asset('packages/just/shapeshifter/js/attributes/medium/js/filebrowser.js') }}"></script>
+        <script src="{{ asset('packages/just/shapeshifter/js/attributes/medium/medium/medium-button.js') }}"></script>
+        <script src="{{ asset('packages/just/shapeshifter/js/attributes/medium/extensions/pastehandler.js') }}"></script>
+        <script src="{{ asset('packages/just/shapeshifter/js/attributes/medium/medium.js') }}"></script>
+        <?php $_SERVER['SCRIPTS_LOADED_MEDIUM'] = true; ?>
+    @endif
 
 @stop
 
 @section('styles')
     @parent
-    <link rel="stylesheet" href="{{ asset('packages/just/shapeshifter/js/attributes/medium/medium/css/medium-editor.css') }}">
-    <link rel="stylesheet" href="{{ asset('packages/just/shapeshifter/js/attributes/medium/medium/css/themes/default.css') }}">
-    <link rel="stylesheet" href="{{ asset('packages/just/shapeshifter/js/attributes/medium/medium.css') }}">
-    <link rel="stylesheet" href="{{ asset('packages/just/shapeshifter/js/attributes/medium/medium-editor-insert-plugin/dist/css/medium-editor-insert-plugin.css') }}">
+    @if (!isset($_SERVER['STYLES_LOADED_MEDIUM']))
+        <link rel="stylesheet" href="{{ asset('packages/just/shapeshifter/js/attributes/medium/medium/css/medium-editor.css') }}">
+        <link rel="stylesheet" href="{{ asset('packages/just/shapeshifter/js/attributes/medium/medium/css/themes/default.css') }}">
+        <link rel="stylesheet" href="{{ asset('packages/just/shapeshifter/js/attributes/medium/medium.css') }}">
+        <link rel="stylesheet" href="{{ asset('packages/just/shapeshifter/js/attributes/medium/medium-editor-insert-plugin/dist/css/medium-editor-insert-plugin.css') }}">
+        <?php $_SERVER['STYLES_LOADED_MEDIUM'] = true; ?>
+    @endif
 @stop
