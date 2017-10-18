885d45d321ed7986b2e631db924183c39281f8f6
Emre Koc <emre.koc@wearejust.com>
2017-10-18 16:02:46 +0200

    Chore: update VideoHelper YouTube ID pattern

--- src/Helpers/VideoHelper.php
+++ src/Helpers/VideoHelper.php
@@ -39,21 +39,7 @@ class VideoHelper
      */
     public static function getYoutubeID($url)
     {
-        $pattern =
-            '%^# Match any YouTube URL
-            (?:https?://)?  # Optional scheme. Either http or https
-            (?:www\.)?      # Optional www subdomain
-            (?:             # Group host alternatives
-              youtu\.be/    # Either youtu.be,
-            | youtube\.com  # or youtube.com
-              (?:           # Group path alternatives
-                /embed/     # Either /embed/
-              | /v/         # or /v/
-              | /watch\?v=  # or /watch\?v=
-              )             # End path alternatives.
-            )               # End host alternatives.
-            ([\w-]{10,12})  # Allow 10-12 for 11 char YouTube id.
-            $%x';
+        $pattern = "/^(?:http(?:s)?:\/\/)?(?:www.)?(?:m.)?(?:youtu.be\/|youtube.com\/(?:(?:watch)?\?(?:.*&)?v(?:i)?=|(?:embed|v|vi|user)\/))([^\?&\"'>]+)/";
 
         preg_match($pattern, $url, $matches);
 
