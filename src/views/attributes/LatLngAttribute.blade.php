fd2e6658b03a5f802f45a99b04354fbe6eea0bdf
Cees van Egmond <cees.vanegmond@wearejust.com>
2017-10-26 11:33:52 +0200

    Chore: Load Google API relative from current protocol

--- src/views/attributes/LatLngAttribute.blade.php
+++ src/views/attributes/LatLngAttribute.blade.php
@@ -1,4 +1,4 @@
-<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+<script type="text/javascript" src="//maps.google.com/maps/api/js?sensor=false"></script>
 <label class="form-group js-placeholder js-latlngattribute" for="{{$name}}">
     <span class="form-group-content">
         <span class="form-label">
